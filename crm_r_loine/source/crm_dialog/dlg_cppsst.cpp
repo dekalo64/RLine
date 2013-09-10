0a821a2055cf2f142826e3f98d28d7ed5b8782aa
yaroslav_kirichok <kirichok.jaroslav@gmail.com>
2013-09-10 14:34:19 +0300

    [*] - Мелкие правки диалогового окна

--- crm_r_loine/source/crm_dialog/dlg_cppsst.cpp
+++ crm_r_loine/source/crm_dialog/dlg_cppsst.cpp
@@ -3,44 +3,48 @@
 #include "source/crm_dialog/dlg_cppsst.h"
 #include "source/crm_core/core_dictionarycore.h"
 
-DictionaryDialog::DictionaryDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::DictionaryDialog)
+CCppsstDialog::CCppsstDialog(QWidget *parent) :
+      QDialog(parent)
+    , enableSave(false)
+    , ui(new Ui::CCppsstDialog)
 {
-    this->setWindowFlags(Qt::Drawer);
+    setWindowFlags(Qt::Drawer);
+    setModal(true);
+
     ui->setupUi(this);
 
-    connect(ui->buttonCancel, SIGNAL(clicked()),            this, SLOT(close()));
-    connect(ui->lineEditItem,  SIGNAL(textEdited(QString)),  this, SLOT(slotCurrentChanged()));
-    connect(ui->comboBoxIcon,   SIGNAL(activated(int)),       this, SLOT(slotCurrentChanged()));
-    connect(ui->checkBoxActual, SIGNAL(clicked()),            this, SLOT(slotCurrentChanged()));
-    connect(ui->buttonSave,   SIGNAL(clicked()),            this, SLOT(slotSaveDataChanged()));
+    connect(ui->buttonCancel, SIGNAL(clicked()), this, SLOT(close()));
+    connect(ui->lineEditName,  SIGNAL(textEdited(QString)), this, SLOT(slotCurrentChanged()));
+    connect(ui->comboBoxIcon,   SIGNAL(activated(int)), this, SLOT(slotCurrentChanged()));
+    connect(ui->checkBoxActual, SIGNAL(clicked()), this, SLOT(slotCurrentChanged()));
+    connect(ui->buttonSave,   SIGNAL(clicked()), this, SLOT(slotSaveDataChanged()));
 
     updateActions();
 }
 
-DictionaryDialog::~DictionaryDialog()
+CCppsstDialog::~CCppsstDialog()
 {
     delete ui;
 }
 
-void DictionaryDialog::closeEvent(QCloseEvent *)
+void CCppsstDialog::closeEvent(QCloseEvent *)
 {
     CDictionaryCore::clearEditDialog(this);
+    enableSave = false;
 }
 
-void DictionaryDialog::showEvent(QShowEvent *)
+void CCppsstDialog::showEvent(QShowEvent *)
 {
     updateActions();
 }
 
-void DictionaryDialog::slotSaveDataChanged()
+void CCppsstDialog::slotSaveDataChanged()
 {
     emit saveDataChanged();
 }
 
-void DictionaryDialog::updateActions()
+void CCppsstDialog::updateActions()
 {
-    bool m_enableBtnConnect = ((!ui->lineEditItem->text().isEmpty()));
-    ui->buttonSave->setEnabled(m_enableBtnConnect);
+    ui->buttonSave->setEnabled(enableSave);
+    enableSave = ((!ui->lineEditName->text().isEmpty()));
 }
